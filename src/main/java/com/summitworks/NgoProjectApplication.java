d68711403051989e28f4a626badb893894bebc14
Cristian Castillo <66140535+criscas465@users.noreply.github.com>
2021-01-26 22:32:43 -0500

    number 4

--- src/main/java/com/summitworks/NgoProjectApplication.java
+++ src/main/java/com/summitworks/NgoProjectApplication.java
@@ -2,7 +2,7 @@ package com.summitworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello #4
 @SpringBootApplication
 public class NgoProjectApplication {
 
