82e119bd54197f1042a24cc1511bb05150a1abeb
tj94109 <52139368+tj94109@users.noreply.github.com>
2021-01-27 12:33:03 -0800

    updated entity classes for user and set application.properties

--- src/main/java/com/summitworks/entity/User.java
+++ src/main/java/com/summitworks/entity/User.java
@@ -1,9 +1,109 @@
 package com.summitworks.entity;
 
+import java.util.HashSet;
+import java.util.Set;
 
-//Hello from terrance
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 
+@Entity
+@Table(name = "User")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String role;
+
+//	@OneToMany(mappedBy = "user", fetch = FetchType.EAGER,
+//			cascade = CascadeType.ALL)
+	
+//	private Set<Events> events = new HashSet<Events>();
+	
+	public User(int id, String firstName, String lastName, String email,String password, String role) {
+		super();
+		this.id = id;
+		this.setFirstName(firstName);
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public User(){}
+
+	public Set<Events> getEvents() {
+		return events;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", role=" + role + ", events=" + events + "]";
+	}
+	
+	
+
+	
 }
