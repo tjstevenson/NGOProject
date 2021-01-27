7172f8a7f8c1dc51b3b479b0c216bbf9b6b642f4
Cristian Castillo <66140535+criscas465@users.noreply.github.com>
2021-01-27 12:56:41 -0500

    Update EventsRepo.java

--- src/main/java/com/summitworks/repo/EventsRepo.java
+++ src/main/java/com/summitworks/repo/EventsRepo.java
@@ -1,10 +1,9 @@
 package com.summitworks.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.summitworks.entity.Events;
-
-
+@Repository
 public interface EventsRepo extends JpaRepository<Events, Integer> {
 
 }
