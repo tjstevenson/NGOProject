f5d04f2ec7898666836ea791e48f02ac12ca7b6d
tj94109 <52139368+tj94109@users.noreply.github.com>
2021-01-26 18:22:17 -0800

    first commit

---
+++ src/main/java/com/summitworks/NgoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.summitworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/summitworks/NgoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summitworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
