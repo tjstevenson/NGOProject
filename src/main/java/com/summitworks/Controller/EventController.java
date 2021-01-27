e4a05f9dbadf93c4c4430c7eb0ed7d07b7c98dda
Cristian Castillo <66140535+criscas465@users.noreply.github.com>
2021-01-27 17:09:43 -0500

    Update EventController.java

--- src/main/java/com/summitworks/Controller/EventController.java
+++ src/main/java/com/summitworks/Controller/EventController.java
@@ -1,5 +1,20 @@
 package com.summitworks.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class EventController {
+	
+	@RequestMapping("/")
+	public String welcomeHotel(Model model)
+	{
+
+		return "index";
+	}
+	
+	
+	
 
 }
