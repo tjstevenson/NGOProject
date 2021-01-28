e55790eccc142d0db73ab8ad7d6d987edef309b3
tj94109 <52139368+tj94109@users.noreply.github.com>
2021-01-28 11:01:25 -0800

    added userManagement template

--- src/main/java/com/summitworks/Controller/EventController.java
+++ src/main/java/com/summitworks/Controller/EventController.java
@@ -1,4 +1,4 @@
-package com.summitworks.Controller;
+package com.summitworks.controller;
 
 import java.util.Date;
 import java.util.List;
--- src/main/java/com/summitworks/controller/UserController.java
+++ src/main/java/com/summitworks/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.summitworks.controller;
 
-public class UserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.summitworks.entity.User;
+import com.summitworks.repo.UserRepo;
+
+@Controller
+public class UserController implements WebMvcConfigurer {
+
+
+	@Autowired
+	UserRepo UserRepo;
+	
+	@RequestMapping("/UserManagement")
+	public String userManagment(Model model)
+	{
+		List<User> listUsers=UserRepo.findAll();
+		model.addAttribute("user",listUsers);
+		return "UserManagement";
+	}
+	
 }
