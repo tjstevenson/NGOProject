3d3aeba88e1207dbf0a6e142fbf96d851ee9440a
tj94109 <52139368+tj94109@users.noreply.github.com>
2021-01-28 20:55:30 -0800

    added blank login pages

--- src/main/java/com/summitworks/controller/LoginController.java
+++ src/main/java/com/summitworks/controller/LoginController.java
@@ -1,5 +1,14 @@
 package com.summitworks.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class LoginController {
+ 
+    @GetMapping("/login")
+    public String viewLoginPage() {
+        
+        return "login";
+    }
 }
