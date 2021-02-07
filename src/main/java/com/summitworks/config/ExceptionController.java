81b52d3e96fe1e1c5be0d10622d2ad1a2d9c0e12
Cristian Castillo <66140535+criscas465@users.noreply.github.com>
2021-02-07 17:55:37 -0500

    update

---
+++ src/main/java/com/summitworks/config/ExceptionController.java
@@ -0,0 +1,26 @@
+package com.summitworks.config;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest request, Exception e)   {
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Request: " + request.getRequestURL() + " raised " + e);
+        return new ModelAndView("error");
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handleError404(HttpServletRequest request, Exception e)   {
+        Logger.getLogger(getClass().getName()).log(Level.SEVERE, "Request: " + request.getRequestURL() + " raised " + e);
+        return new ModelAndView("404");
+    }
+}
